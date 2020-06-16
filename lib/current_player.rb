de8f6624581923368aeffd1321499fedbc6e91e3
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-16 22:50:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,3 +8,12 @@ def turn_count(board)
  end
  return counter
 end
+
+def current_player(board)
+  counter = turn_count(board)
+  if counter % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
